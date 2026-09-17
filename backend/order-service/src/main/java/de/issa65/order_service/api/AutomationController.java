8017dd41d480cb3d60053f09a8798498fd8200f5
issa65 <ahmad325issa@hotmail.com>
2026-09-17 18:37:49 +0200

    Add Spring Boot automation API

---
+++ backend/order-service/src/main/java/de/issa65/order_service/api/AutomationController.java
@@ -0,0 +1,34 @@
+package de.issa65.order_service.api;
+
+import de.issa65.order_service.service.AutomationService;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/automation")
+public class AutomationController {
+
+    private final AutomationService automationService;
+
+    public AutomationController(AutomationService automationService) {
+        this.automationService = automationService;
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<Map<String, Object>> start(
+            @Valid @RequestBody AutomationRequest request) {
+
+        ProcessInstanceEvent instance =
+                automationService.startAutomation(request.url());
+
+        return ResponseEntity.ok(Map.of(
+                "processInstanceKey", instance.getProcessInstanceKey(),
+                "bpmnProcessId", instance.getBpmnProcessId(),
+                "version", instance.getVersion()
+        ));
+    }
+}
